030b981c649f93c751a7564300682585693afbd3
mostafa-hossam2Z <mohossam973@gmail.com>
2025-06-10 23:38:40 +0300

    Upload profile photo for the user

--- lc_bdd-framework_shaft-Mostafa/src/main/java/pages/CommonPages/Homepage.java
+++ lc_bdd-framework_shaft-Mostafa/src/main/java/pages/CommonPages/Homepage.java
@@ -9,8 +9,9 @@ public class Homepage {
 
    By ClickOnUserPage=By.xpath("//span[contains(text(),'المستخدمين')]");
    By ClickOnDepartmentPage=By.xpath("//span[@class='menu-title'][contains(text(),'الإدارات')]");
-    By RulesPage=By.xpath("//span[contains(text(),'الأدوار')]");
-
+   By RulesPage=By.xpath("//span[contains(text(),'الأدوار')]");
+   By MyProfilePage=By.xpath("//button[@id='user-dropdown-toggle']//img");
+   By Profile=By.xpath("(//a[@class='menu-link px-5 d-flex gap-3 align-items-center'])[1]");
     public void ClickOnUserPage(){
         driver.element().click(ClickOnUserPage);
     }
@@ -22,6 +23,10 @@ public class Homepage {
 
     public void ClickOnRulesPage(){
         driver.element().click(RulesPage);
+    }
+    public void GoToProfile(){
+        driver.element().click(MyProfilePage);
+        driver.element().click(Profile);
     }
 
 
